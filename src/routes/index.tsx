4d4497210f10eef32cc241c0f8f2ff506d4a0250
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-10 20:26:56 +0800

    feat: add Supabase blog fetching functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,32 @@
-import Navbar from '@/components/Navbar';
 import { createFileRoute } from '@tanstack/react-router';
+import { supabase } from '@/supabase-client';
+import Navbar from '@/components/Navbar';
+
+const fetchBlogs = async () => {
+  const { error, data } = await supabase
+    .from('blogs')
+    .select('*')
+    .order('created_at', { ascending: true });
+
+  if (error) {
+    console.error('Error loading blogs: ', error.message);
+    return;
+  }
+
+  return data;
+};
 
 export const Route = createFileRoute('/')({
   component: App,
+  loader: async () => {
+    return fetchBlogs();
+  },
 });
 
 function App() {
+  const blogs = Route.useLoaderData();
+  console.log(blogs);
+
   return (
     <>
       <Navbar />
