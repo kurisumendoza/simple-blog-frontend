bcbf9bddbb3c5005aff5de77ae2462b75a2cf4a4
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-11 03:19:43 +0800

    feat: add pagination UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { supabase } from '@/lib/supabase-client';
 import type { BlogEntry } from '@/types/BlogEntry';
 import BlogList from '@/components/BlogList';
 
 const fetchBlogs = async () => {
-  const { error, data } = await supabase
+  const { error, data, count } = await supabase
     .from('blogs')
-    .select('*')
-    .order('created_at', { ascending: false });
+    .select('*', { count: 'exact' })
+    .order('created_at', { ascending: false })
+    .range(0, 4);
 
   if (error) {
     console.error('Error loading blogs: ', error.message);
     return;
   }
 
-  return data;
+  const totalPages = count ? Math.ceil(count / 5) : 0;
+
+  return { data, count, totalPages };
 };
 
 export const Route = createFileRoute('/')({
@@ -25,7 +28,39 @@ export const Route = createFileRoute('/')({
 });
 
 function App() {
-  const blogs: BlogEntry[] = Route.useLoaderData();
+  const {
+    data: blogs,
+    count,
+    totalPages,
+  }: {
+    data: BlogEntry[];
+    count: number;
+    totalPages: number;
+  } = Route.useLoaderData();
 
-  return <BlogList blogs={blogs} />;
+  return (
+    <>
+      <BlogList blogs={blogs} />
+
+      {count > 5 && (
+        <div className="flex justify-center items-center gap-5 pt-8">
+          <button className="opacity-50 border rounded py-1 px-2" disabled>
+            Back
+          </button>
+          <div className="flex gap-2">
+            <span className="font-bold">1</span>
+            <span>of</span>
+            <span className="font-bold">{totalPages}</span>
+          </div>
+          <Link
+            to="/page/$pageId"
+            params={{ pageId: '2' }}
+            className="border rounded py-1 px-2 cursor-pointer"
+          >
+            Next
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
