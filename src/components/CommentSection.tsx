deeb14d4a8ab6503f597f848e0e7fe3346226572
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-15 19:12:26 +0800

    feat: add UI for comment form

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -1,6 +1,9 @@
 import { useSelector } from 'react-redux';
 import type { RootState } from '@/store/store';
 import { Link } from '@tanstack/react-router';
+import { useState } from 'react';
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
 
 const CommentSection = ({
   blogId,
@@ -9,6 +12,9 @@ const CommentSection = ({
   blogId: number;
   ownerId: string;
 }) => {
+  const [body, setBody] = useState('');
+  const [image, setImage] = useState('');
+
   const { user: currentUser, authId: currentUserAuthId } = useSelector(
     (state: RootState) => state.auth
   );
@@ -29,6 +35,42 @@ const CommentSection = ({
           </Link>
         </div>
       )}
+
+      {currentUser && (
+        <form className="flex flex-col gap-2 my-3">
+          <textarea
+            name=""
+            value={body}
+            placeholder="Leave a comment"
+            maxLength={300}
+            onChange={(e) => setBody(e.target.value)}
+            className="w-full border rounded-md py-2 px-3 resize-none"
+            required
+          />
+
+          <div className="flex items-center">
+            <p>Add image (optional)</p>
+            <label
+              htmlFor="image"
+              className="bg-blue-300 rounded-md text-gray-950 px-3 py-1 ml-5 cursor-pointer"
+            >
+              {image ? 'Change Image' : 'Choose File'}
+            </label>
+            <input
+              id="image"
+              type="file"
+              accept="image/png, image/jpeg"
+              onChange={() => {}}
+              className="hidden"
+            />
+          </div>
+
+          <button className="bg-blue-400 w-[40%] px-3 py-0.5 rounded-md text-gray-800 cursor-pointer hover:bg-blue-800 hover:text-gray-100 transition">
+            Submit
+          </button>
+        </form>
+      )}
+
       {/* {!isOwner && <p>not my blog</p>}
       {isOwner && <p>my blog</p>} */}
     </div>
